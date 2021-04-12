c63789c10ae23385f7685e3f45cd750905e8db53
Christian Cesar <christiancnp@gmail.com>
2021-04-12 14:40:23 -0400

    include controller provider list dates

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,13 +1,29 @@
 import ProvidersController from '@modules/appointments/controllers/ProvidersController';
+import ProviderDayAvailabilityController from '@modules/appointments/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/appointments/controllers/ProviderMonthAvailabilityController';
+
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import { Router } from 'express';
 
 const providersRouter = Router();
 
 const providersController = new ProvidersController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
