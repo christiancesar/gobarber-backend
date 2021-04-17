8153b0912c8ae3d2024117fa789f81e9a62844e2
Christian Cesar <christiancnp@gmail.com>
2021-04-16 22:15:55 -0400

    fixed erros

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
-import { getDate, getDaysInMonth } from 'date-fns';
+import { isAfter, getDate, getDaysInMonth } from 'date-fns';
+
 import { inject, injectable } from 'tsyringe';
 import IAppointmentRepository from '../repositories/IAppointmentsRepository';
 
@@ -37,13 +38,15 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
